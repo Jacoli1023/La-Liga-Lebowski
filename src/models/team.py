6d64465a43dcdc4a1b17adb86c4c737a500b6d12
Jacob Oliver <jacoli1023@gmail.com>
2025-08-25 19:14:25 -0500

    Implemented logic for removing a player. Fleshed out logic for adding a player.
    There is a lot of extra logic that goes into the financial side of
    things. So I deferred quite a bit of that logic to future service
    classes that will (hopefully) be able to handle it all.
    
    Future functionality to implement within the team class: moving a player
    from one roster to another, separate active roster from bench, check
    validity of starting lineup, display rosters.

--- src/models/team.py
+++ src/models/team.py
@@ -15,31 +15,75 @@ class Team:
 
 
     def add_player(self, player, roster_type='active'):
-        if roster_type == 'active':
-            roster_max = LEAGUE_SETTINGS['max_roster_size']
-        elif roster_type == 'practice_squad':
-            roster_max = LEAGUE_SETTINGS['practice_squad_slots']
-        elif roster_type == 'IR':
-            roster_max = LEAGUE_SETTINGS['injured_reserve_slots']
-        else:
-            raise ValueError(f"Invalid roster type: {roster_type}")
+        """Add contracted player to this team's roster"""
+        roster_max = self._get_roster_max(roster_type)
 
+        # Checks if player is available
+        if not player.is_available():
+            raise ValueError(f"Cannot add player: {player.name} is unavailable")
+
+        # Checks if there is enough room on the roster
         if self.get_roster_size(roster_type) >= roster_max:
             raise ValueError(f"Cannot add player: {roster_type} roster is full")
 
-        if self.can_afford(player, roster_type):
-            self.roster[roster_type].append(player)
-            player.fantasy_team = self.name
-            player.roster_status = roster_type
+        # Adds player to specified roster, and updates player status
+        self.roster[roster_type].append(player)
+        player.roster_status = roster_type
+        player.fantasy_team = self.name
+        # TODO: Implement bids and contracts
+
+
+    def remove_player(self, player):
+        """Remove player from roster, handle salary cap consequences"""
+        if not player.fantasy_team == self.name:
+            raise ValueError(f"Cannot remove player: {player.name} is not part of your team!")
+
+        for roster_list in self.roster.values():
+            if player in roster_list:
+                roster_list.remove(player)
+                self.dead_money += player.salary
+                player.roster_status = "free_agent"
+                player.fantasy_team = None
+                return
+                # TODO: Future dead money penalties via ContractService
+                #       Adjust player salary & contract after dropping
+        else:
+            raise ValueError(f"Cannot remove player: {player.name} not found in roster!")
 
     
     def get_total_salary_used(self):
-        total_sal = 0
+        total_salary = 0
+
+        # Add up effective salaries from all roster types
         for roster_list in self.roster.values():
             for player in roster_list:
-               total_sal += player.get_effective_salary() 
-        return total_sal
+               total_salary += player.get_effective_salary() 
+
+        # Add dead money from dropped players
+        total_salary += self.dead_money
+
+        return total_salary
+
+
+    def get_remaining_cap(self):
+        return self.salary_cap - self.get_total_salary_used()
+
+
+    def get_roster_size(self, roster_type):
+        return len(self.roster[roster_type])
         
 
     def can_afford(self, player, roster_type='active'):
         pass
+
+
+    def _get_roster_max(self, roster_type):
+        """Get maximum roster size for given roster type"""
+        if roster_type == 'active':
+            return LEAGUE_SETTINGS['max_roster_size']
+        elif roster_type == 'practice_squad':
+            return LEAGUE_SETTINGS['practice_squad_slots']
+        elif roster_type == 'IR':
+            return LEAGUE_SETTINGS['injured_reserve_slots']
+        else:
+            raise ValueError(f"Invalid roster type: {roster_type}")
